3f43a900cd70554b9cad01a291beb8bf7f1f2b0d
Pradeep567iaf <60265769+Pradeep567iaf@users.noreply.github.com>
2024-07-10 13:08:23 +0000

    added the custom user and post model

--- Flask_Blog/flask_blog.py
+++ Flask_Blog/flask_blog.py
@@ -1,8 +1,36 @@
 from flask import Flask, render_template, url_for, flash, redirect
+from sqlalchemy import Nullable
 from forms import RegistrationForm, LoginForm
-
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(20), unique=True, null=False)
+  email = db.Column(db.String(120), unique=True, null=False)
+  image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+  password = db.Column(db.String(60), nullable=False)
+  Post = db.relationship('Post',backref='author',lazy=True)
+
+  def __repr__(self):
+    return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Post(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  title = db.Column(db.String(100), nullable=False)
+  date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+  content = db.Column(db.Text, nullable=False)
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+  def __repr__(self):
+    return f"Post('{self.title}','{self.date_posted}')"
+
 
 app.config['SECRET_KEY'] = '588f61dc98d89b26a8f700fad55462de'
 
--- app.py
+++ app.py
@@ -14,3 +14,41 @@ if __name__ == "__main__":
 # set DEBUG = 1
 # set FLASK_APP = app.py
 # python app.py
+
+
+ # from flaskblog import db
+ # db.create_all()
+# from flaskblog import User, Post
+# user_1 = User(username='Corey', email="C@demo.com", password='password')
+# db.session.add(user_1)
+# user_2 = User(username='JohnDoe', email="jd@demo.com", password='password')
+# db.session.add(user_2)
+# db.session.commit()
+# User.query.all()
+# User.query.first()
+# User.query.filter_by(username='Corey').all()
+# User.query.filter_by(username='Corey').all().first()
+# user = User.query.filter_by(username='Corey').all().first()
+# user.id
+# user = User.query.get(1)
+# user.posts
+# user.id
+# post_1 = Post(title='Blog Post 1',content = "First Post content", user_id = user.id)
+# post_2 = Post(title='Blog Post 2',content = "Second Post content", user_id = user.id)
+# db.session.add(post_1)
+# db.session.add(post_2)
+# db.session.commit()
+# user.posts
+# for post in user.posts:
+#   print(post.title)
+# post = Post.query.first()
+# post.user_id
+# post.author
+# db.drop_all()
+# db.create_all()
+# User.query.all()
+# Post.query.all()
+
+
+
+
